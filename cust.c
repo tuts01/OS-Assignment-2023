b84d52edf10666d4f89e0c27dde045233c588d98
tuts01 <65433301+tuts01@users.noreply.github.com>
2023-04-24 13:54:13 +0800

    Created cust.c - file which holds the customer() function

---
+++ cust.c
@@ -0,0 +1,15 @@
+/**
+ * @file            cust.c
+ * @author          Tristan S. Tutungis
+ * @date_created    18/04/2023
+ * @last_modified   
+ * @description     Contains a function that periodically reads an entry from
+ *                  file and adds it to a FIFO queue
+*/
+
+#include "cust.h"
+
+void* customer(void *queue)
+{
+    
+}
