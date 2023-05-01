a144f81c8535f201b0215b850a5b2f75aa41d2c7
tuts01 <65433301+tuts01@users.noreply.github.com>
2023-05-01 09:19:07 +0800

    Implemented customer()

--- cust.c
+++ cust.c
@@ -7,9 +7,60 @@
  *                  file and adds it to a FIFO queue
 */
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <time.h>
 #include "cust.h"
+#include "queue.h"
+#include "fileio.h"
 
-void* customer(void *queue)
+extern long a_time;
+extern pthread_mutex_t queue_mutex;
+extern pthread_mutex_t file_mutex;
+
+void* customer(void *queue_ptr)
 {
+    FILE* c_file = open(INFILENAME, "r");
+    FILE* r_log = open(OUTFILENAME, "a");
+    bool done = false;
+    queue_t* queue = (queue_t*)queue_ptr;
+    cust_t* cust;
+    char service;
     
+    while (!done)
+    {
+        cust = readCustFile(c_file, &done);
+        
+        if(cust = NULL) done = true;
+        else
+        {
+            switch(cust->service)
+            {
+                case W:
+                    service = 'W';
+                    break;
+                
+                case D:
+                    service = 'D';
+                    break;
+
+                case I:
+                    service = 'I';
+                    break;
+            }
+
+            struct tm* curtime = localtime(time(NULL));
+
+            pthread_mutex_lock(&queue_mutex);
+
+            fprintf(r_log, "%s\n%d: %c\nArrival Time: %d:%d:%d\n%s", DIVIDER,
+                    cust->custNo, service, curtime->tm_hour, curtime->tm_min,
+                    curtime->tm_sec, DIVIDER);
+            add(queue, cust);
+
+            pthread_mutex_unlock(&queue_mutex);
+            sleep(a_time);
+        }
+    }
 }
