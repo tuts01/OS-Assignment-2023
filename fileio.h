d98c50a8a7d8d9d00bac5201f6fb49e07c8cb408
tuts01 <65433301+tuts01@users.noreply.github.com>
2023-05-01 09:17:35 +0800

    Added function declarations and a macro for a line divider

--- fileio.h
+++ fileio.h
@@ -2,8 +2,13 @@
 #define FILEIO_H
 
 #include "cust.h"
+
 #define MAXLINELEN 5
 #define INFILENAME "c_file"
 #define OUTFILENAME "r_log"
+#define DIVIDER "-----------------------------------------------------------------------"
+
+FILE* open(char* name, char* mode);
+cust_t* readCustFile(FILE* file, _Bool* eof);
 
 #endif
